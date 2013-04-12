164257816441b9e13e899c0b440bd3500947979b
Kornelius Kalnbach <murphy@rubychan.de>
2013-04-12 14:55:43 +0200

    fix session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-White::Application.config.session_store :encrypted_cookie_store, key: '_White_session'
+White::Application.config.session_store :cookie_store, key: '_white_session'
