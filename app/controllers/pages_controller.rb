7ebe6829b6b4e9106bc172f6bcb0d07fd07417ee
Kornelius Kalnbach <murphy@rubychan.de>
2013-04-30 08:31:02 +0200

    edit the page zoom factor using prompt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   skip_before_filter :login_required
   
-  before_action :set_section, :only => :show
-  before_action :set_page,    :only => :show
+  before_action :set_section, :only => [:show, :update]
+  before_action :set_page,    :only => [:show, :update]
   
   def index
     @section = Section.welcome_section
@@ -12,6 +12,13 @@ class PagesController < ApplicationController
   end
   
   def show
+  end
+  
+  def update
+    @page.images_zoom_factor = params[:images_zoom_factor]
+    @page.save!
+    
+    reload_page
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ White::Application.routes.draw do
   resource :session, only: [:create, :destroy]
   
   resources :sections, only: [], path: '' do
-    resources :pages, only: [:show, :index], path: '' do
+    resources :pages, only: [:show, :update], path: '' do
       resources :images, only: [:create, :destroy] do
         collection do
           patch :reorder
