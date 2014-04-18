ce20be09e7c501d10567f40c2905108e101df75d
Kornelius Kalnbach <murphy@rubychan.de>
2014-04-18 22:51:49 +0200

    tweak deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app), in: :sequence, wait: 1 do
       # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
     end
@@ -50,12 +50,12 @@ namespace :deploy do
   after :publishing, :restart
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+    # on roles(:web), in: :groups, limit: 3, wait: 10 do
+    #   # Here we can do anything such as:
+    #   # within release_path do
+    #   #   execute :rake, 'cache:clear'
+    #   # end
+    # end
   end
 
   after :finishing, "deploy:cleanup"
