973367d381b8c42331780a05bbacdc3475c9f2d4
Kornelius Kalnbach <murphy@rubychan.de>
2014-04-19 13:08:49 +0200

    add analytics to layout

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml app/views/layouts/_analytics.html.haml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets public/system system}
