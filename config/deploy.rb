3530f5db34958c3c263a192df142423df4e9e59e
Kornelius Kalnbach <murphy@rubychan.de>
2015-01-14 20:44:10 +0100

    update cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+# config valid only for Capistrano 3
+lock '3.3.5'
 
 set :application, 'White'
 set :repo_url, 'git@github.com:korny/White.git'
